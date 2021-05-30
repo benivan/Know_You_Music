700631690335ef577b5c72c6bfded8d646c3962f
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-30 12:11:42 +0530

    "main page is up"

--- app/src/main/java/com/aiden/knowyoumusic/GenreListFragment.kt
+++ app/src/main/java/com/aiden/knowyoumusic/GenreListFragment.kt
@@ -7,38 +7,33 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.aiden.knowyoumusic.databinding.FragmentGenreBinding
+import com.aiden.knowyoumusic.databinding.FragmentGenreListBinding
 import com.aiden.knowyoumusic.model.GenreElement
 import com.aiden.knowyoumusic.utils.getJsonDataFromAsset
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class MainFragment : Fragment() {
-    private var _binding: FragmentGenreBinding? = null
+class GenreListFragment : Fragment() {
+    private var _binding: FragmentGenreListBinding? = null
 
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
-        _binding = FragmentGenreBinding.inflate(inflater,container,false)
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentGenreListBinding.inflate(inflater,container,false)
         return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lateinit var  recyclerView: RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = GenreAdapter(getData())
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = GenreAdapter(getData())
     }
 
     private fun getData(): List<GenreElement> {
--- app/src/main/java/com/aiden/knowyoumusic/MainActivity.kt
+++ app/src/main/java/com/aiden/knowyoumusic/MainActivity.kt
@@ -2,10 +2,26 @@ package com.aiden.knowyoumusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.aiden.knowyoumusic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+         navController = navHostFragment.navController
+
+
     }
 }
