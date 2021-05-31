ed525aef21612ff9cde632a6c8d4f8e18c56a1ac
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-31 22:23:24 +0530

    "make model parcelize"

--- app/src/main/java/com/aiden/knowyoumusic/model/Genre.kt
+++ app/src/main/java/com/aiden/knowyoumusic/model/Genre.kt
@@ -1,5 +1,9 @@
 package com.aiden.knowyoumusic.model
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
 
 typealias Genre = ArrayList<GenreElement>
 
@@ -10,15 +14,20 @@ data class GenreElement(
     val genrelist: List<Genrelist>
 )
 
+
+@Parcelize
 data class Genrelist(
     val name: String,
     val year: String,
+    @SerializedName("supergenre-first-tag")
     val supergenreFirstTag: String,
     val supergenreSecondTag: List<String>? = null,
     val description: String,
     val songlist: List<String>,
     val playlistID: String,
+    @SerializedName("playlist-link")
     val playlistLink: String,
+    @SerializedName("spotify-playlist-link")
     val spotifyPlaylistLink: String
-)
+): Parcelable
 
