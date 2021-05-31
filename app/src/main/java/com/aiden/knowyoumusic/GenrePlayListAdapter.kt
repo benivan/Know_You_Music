41bf55dac78aba93a603e125ae207a4699141775
Vikash Kumar <itsmevikash09@gmail.com>
2021-05-31 22:25:55 +0530

    "playlist screen done "

---
+++ app/src/main/java/com/aiden/knowyoumusic/GenrePlayListAdapter.kt
@@ -0,0 +1,37 @@
+package com.aiden.knowyoumusic
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.aiden.knowyoumusic.databinding.SongItemViewBinding
+
+class GenrePlayListAdapter(private val songList: List<String>):
+    RecyclerView.Adapter<GenrePlayListAdapter.GenrePlayListViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenrePlayListViewHolder {
+       val binding = SongItemViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return GenrePlayListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: GenrePlayListViewHolder, position: Int) {
+        val currentItem = songList[position]
+        return holder.bind(currentItem)
+    }
+
+    override fun getItemCount(): Int {
+      return songList.size
+    }
+
+
+    inner  class GenrePlayListViewHolder(private val binding: SongItemViewBinding) :RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentItem: String) {
+            binding.songItem.text = currentItem
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/aiden/knowyoumusic/PlayListAndGenreFragment.kt
@@ -0,0 +1,66 @@
+package com.aiden.knowyoumusic
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.aiden.knowyoumusic.databinding.GenreDetailPlaylistBinding
+
+class PlayListAndGenreFragment : Fragment() {
+
+
+    private var _binding: GenreDetailPlaylistBinding? = null
+    val args: PlayListAndGenreFragmentArgs by navArgs()
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        _binding = GenreDetailPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvSongList.layoutManager = LinearLayoutManager(context)
+        binding.rvSongList.adapter = GenrePlayListAdapter(args.subGenrePlayList.songlist)
+        binding.tvGenreDescription.text = args.subGenrePlayList.description
+        binding.tvGenreName.text = args.subGenrePlayList.name
+        onYoutubeButtonClick()
+        onSpotifyButtonClick()
+
+    }
+
+    private fun onSpotifyButtonClick() {
+        binding.btnSpotify.setOnClickListener {
+            val queryUrl: Uri = Uri.parse(args.subGenrePlayList.spotifyPlaylistLink)
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            requireActivity().startActivity(intent)
+        }
+    }
+
+    private fun onYoutubeButtonClick() {
+        binding.btnYoutube.setOnClickListener {
+            val queryUrl: Uri = Uri.parse(args.subGenrePlayList.playlistLink)
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            requireActivity().startActivity(intent)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
