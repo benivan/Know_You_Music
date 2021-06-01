aba5149f04226adb7992a25ed5701996eb196756
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-01 19:21:43 +0530

    "nav bar back button added"

--- app/src/main/java/com/aiden/knowyoumusic/MainActivity.kt
+++ app/src/main/java/com/aiden/knowyoumusic/MainActivity.kt
@@ -21,7 +21,13 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
          navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
 
 
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
